642c444ecfca69157f1a884b363dce06c98da16b
Angela Su <su_ange@bentley.edu>
2017-11-11 17:13:04 -0500

    added map activity

--- app/src/main/java/com/example/su_ange/app/MainActivity.java
+++ app/src/main/java/com/example/su_ange/app/MainActivity.java
@@ -1,13 +1,31 @@
 package com.example.su_ange.app;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import static android.R.attr.button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent intent;
+        intent = new Intent(this, MapsActivity.class);
+        startActivity(intent);
+
     }
 }
