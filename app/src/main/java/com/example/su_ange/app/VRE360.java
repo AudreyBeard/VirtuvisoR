89b5291847814d1bb0310271c6f96aa9b448448c
joshuabeard <joshuabeard92@gmail.com>
2017-11-12 10:28:52 -0500

    latlng support

--- app/src/main/java/com/example/su_ange/app/VRE360.java
+++ app/src/main/java/com/example/su_ange/app/VRE360.java
@@ -4,15 +4,18 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-
+import com.google.android.gms.maps.*;
 import com.google.android.gms.maps.OnStreetViewPanoramaReadyCallback;
 import com.google.android.gms.maps.StreetViewPanorama;
+import com.google.android.gms.maps.StreetViewPanoramaFragment;
 import com.google.android.gms.maps.SupportStreetViewPanoramaFragment;
 import com.google.android.gms.maps.model.LatLng;
 import android.widget.EditText;
+
 
     public class VRE360 extends AppCompatActivity {
 
@@ -20,7 +23,7 @@ import android.widget.EditText;
         //private final Geocoder geocoder = new Geocoder();
         //GeocoderRequest geocoderRequest = new GeocoderRequestBuilder().setAddress()
         // Boston, MA
-        private final LatLng BOSTON = new LatLng(42.33141,-71.099396);
+        private LatLng location;
 
 
         @Override
@@ -28,7 +31,9 @@ import android.widget.EditText;
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_vre360);
             Intent intent = getIntent();
-            String location = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+            String latLngString = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+            String[] latLngParts = latLngString.split(",");
+            location = new LatLng(Float.parseFloat(latLngParts[0]), Float.parseFloat(latLngParts[1]));
             
             SupportStreetViewPanoramaFragment streetViewPanoramaFragment =
                     (SupportStreetViewPanoramaFragment)
@@ -40,9 +45,9 @@ import android.widget.EditText;
                             // Only set the panorama to BOSTON on startup (when no panoramas have been
                             // loaded which is when the savedInstanceState is null).
                             if (savedInstanceState == null) {
-                                panorama.setPosition(BOSTON);
+                                panorama.setPosition(location);
                             }
                         }
                     });
         }
-}
+    }
